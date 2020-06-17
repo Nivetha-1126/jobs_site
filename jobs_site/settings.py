54f3fdd273101efb1e3f93400f150573cd25cf77
Nivetha-1126 <nivethashanmugam26@gmail.com>
2020-06-17 12:22:35 +0530

    Setting file updated

--- jobs_site/settings.py
+++ jobs_site/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'qk+)akdsdh)1-=6c44_)%bo9a%@2^l1n@&+c$jghfjf4ooi=5r'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nivethaawesomework.herokuapp.com']
 
 
 # Application definition
